422a8459fe16ac2ff894edfc6f0557c8bbfa468d
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-10-31 14:57:46 +0000

    Update MatUI.jsx
    moooore UI

--- src/MatUI.jsx
+++ src/MatUI.jsx
@@ -6,15 +6,21 @@ import AddIcon from "@mui/icons-material/Add";
 import Rating from "@mui/material/Rating";
 import Select from "@mui/material/Select";
 import Typography from "@mui/material/Typography";
-import InputLabel from "@mui/material/InputLabel";
+import Slider from "@mui/material/Slider";
 import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+import Switch from "@mui/material/Switch";
+import TextField from "@mui/material/TextField";
 
 export default function MatUI() {
   return (
     <div>
       <Button />
       <Checkbox />
-      <Fab color="primary" aria-label="add">
+      <Fab color="success" aria-label="add">
         <AddIcon />
       </Fab>
       <Typography component="legend">Controlled</Typography>
@@ -24,6 +30,15 @@ export default function MatUI() {
         <MenuItem value={20}>OK</MenuItem>
         <MenuItem value={30}>Bad</MenuItem>
       </Select>
+      <Box sx={{ width: 400, height: 200 }}>
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+          <VolumeDown />
+          <Slider aria-label="Volume" />
+          <VolumeUp />
+        </Stack>
+      </Box>
+      <Switch defaultChecked />
+      <TextField id="outlined-basic" label="Type In Me" variant="outlined" />
     </div>
   );
 }
