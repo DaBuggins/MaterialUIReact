4f66c7366d40674e3e600aa00c423be4d0060b78
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-11-07 16:27:12 +0000

    Update MatUI.jsx

--- src/MatUI.jsx
+++ src/MatUI.jsx
@@ -14,22 +14,33 @@ import VolumeDown from "@mui/icons-material/VolumeDown";
 import VolumeUp from "@mui/icons-material/VolumeUp";
 import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
+import ToggleButtons from "./ToggleGroup";
+import Avatar from "@mui/material/Avatar";
+import MailIcon from "@mui/icons-material/Mail";
+import Badge from "@mui/material/Badge";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 
 export default function MatUI() {
   return (
     <div>
-      <Button />
-      <Checkbox />
-      <Fab color="success" aria-label="add">
-        <AddIcon />
-      </Fab>
-      <Typography component="legend">Controlled</Typography>
-      <Rating name="simple-controlled" />
-      <Select>
-        <MenuItem value={10}>Good</MenuItem>
-        <MenuItem value={20}>OK</MenuItem>
-        <MenuItem value={30}>Bad</MenuItem>
-      </Select>
+      <Box sx={{ width: 400, height: 200 }}>
+        <Button />
+        <Checkbox />
+        <Fab color="success" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Box>
+      <Box>
+        <Typography component="legend">Controlled</Typography>
+        <Rating name="simple-controlled" />
+        <Select>
+          <MenuItem value={10}>Good</MenuItem>
+          <MenuItem value={20}>OK</MenuItem>
+          <MenuItem value={30}>Bad</MenuItem>
+        </Select>
+      </Box>
       <Box sx={{ width: 400, height: 200 }}>
         <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
           <VolumeDown />
@@ -39,6 +50,16 @@ export default function MatUI() {
       </Box>
       <Switch defaultChecked />
       <TextField id="outlined-basic" label="Type In Me" variant="outlined" />
+      <Box>
+        <ToggleButtons />
+      </Box>
+      <Avatar sx={{ bgcolor: "orange" }}>N</Avatar>
+      <Badge badgeContent={4} color="secondary">
+        <MailIcon color="action" />
+      </Badge>
+      <Chip label="primary" color="primary" />
+      <Divider />
+      <CatchingPokemonIcon />
     </div>
   );
 }
